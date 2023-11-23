71afe364ba0183481234b32fde2f0e4c49bd2122
Dave Burke <dburke84@gmail.com>
2023-11-23 13:13:02 -0600

    Use local storage for store

--- src/stores/trainingMax.ts
+++ src/stores/trainingMax.ts
@@ -1,11 +1,11 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
 
 export const useTrainingMaxStore = defineStore('trainingMax', () => {
-  const squatMax = ref(0)
-  const benchMax = ref(0)
-  const deadMax = ref(0)
-  const pressMax = ref(0)
+  const squatMax = useLocalStorage('squatMax', 0)
+  const benchMax = useLocalStorage('benchMax', 0)
+  const deadMax = useLocalStorage('deadMax', 0)
+  const pressMax = useLocalStorage('pressMax', 0)
 
   return { squatMax, benchMax, deadMax, pressMax }
 })
