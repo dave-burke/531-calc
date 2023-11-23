c13fd60212435d306e7ee88a37886ded1c52788c
Dave Burke <dburke84@gmail.com>
2023-11-23 11:22:18 -0600

    Use vite-plugin-vuetify

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,8 @@ import router from './router'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+const vuetify = createVuetify({ })
 
 const app = createApp(App)
 
