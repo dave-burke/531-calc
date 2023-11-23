edf6ae2ebc18c7fb91589a890b5dc718006330ad
Dave Burke <dburke84@gmail.com>
2023-11-23 12:11:42 -0600

    Add day/lift toggles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
 import { useTrainingMaxStore } from '@/stores/trainingMax'
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
 const { squatMax, benchMax, deadMax, pressMax } = storeToRefs(useTrainingMaxStore())
+
+const day = ref(5)
+
+enum Lift {
+    Squat,
+    Dead,
+    Bench,
+    Press,
+}
+
+const lift = ref(Lift.Squat)
 
 </script>
 
 <template>
 <h1>Home</h1>
+<v-btn-toggle v-model="day" shaped mandatory class="my-2">
+    <v-btn :value="5">5</v-btn>
+    <v-btn :value="3">3</v-btn>
+    <v-btn :value="1">1</v-btn>
+</v-btn-toggle>
+<v-btn-toggle v-model="lift" shaped mandatory class="my-2">
+    <v-btn :value="Lift.Squat">Squat</v-btn>
+    <v-btn :value="Lift.Bench">Bench</v-btn>
+    <v-btn :value="Lift.Dead">Deadlift</v-btn>
+    <v-btn :value="Lift.Press">Press</v-btn>
+</v-btn-toggle>
 <v-table>
     <tbody>
         <tr>
