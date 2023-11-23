7dbd1b9fe4c0584e8a249f8202861f977e751af2
Dave Burke <dburke84@gmail.com>
2023-11-23 11:54:32 -0600

    Add Settings page with store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTrainingMaxStore } from '@/stores/trainingMax'
+import { storeToRefs } from 'pinia';
 
-<template>Home</template>
+const { squatMax, benchMax, deadMax, pressMax } = storeToRefs(useTrainingMaxStore())
+
+</script>
+
+<template>
+<h1>Home</h1>
+<v-table>
+    <tbody>
+        <tr>
+            <td>Squat</td><td>{{ squatMax }}</td>
+        </tr>
+        <tr>
+            <td>Bench Press</td><td>{{ benchMax }}</td>
+        </tr>
+        <tr>
+            <td>Deadlift</td><td>{{ deadMax }}</td>
+        </tr>
+        <tr>
+            <td>Overhead Press</td><td>{{ pressMax }}</td>
+        </tr>
+    </tbody>
+</v-table>
+
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useTrainingMaxStore } from '@/stores/trainingMax'
+
+const rules = [
+    value => {
+        // string of digits that can't start with zero
+        if(/^[1-9]\d*$/.test(value)) return true;
+        return "You must enter a positive whole number"
+    }
+]
+
+const { squatMax, benchMax, deadMax, pressMax } = storeToRefs(useTrainingMaxStore())
+
+</script>
 <template>
-    Settings
+    <h1>Training Max</h1>
+    <v-form @submit.prevent>
+        <v-text-field label="Squat" v-model="squatMax" :rules="rules"></v-text-field>
+        <v-text-field label="Bench Press" v-model="benchMax" :rules="rules"></v-text-field>
+        <v-text-field label="Deadlift" v-model="deadMax" :rules="rules"></v-text-field>
+        <v-text-field label="Overhead Press"  v-model="pressMax" :rules="rules"></v-text-field>
+    </v-form>
 </template>
