682986ccc00fefb3867a915f35f7eeddbc100cc4
Dave Burke <dburke84@gmail.com>
2023-11-24 18:31:41 -0600

    Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,29 @@ export default defineConfig({
         short_name: "531 Calc",
         description: "A calculator for the 5/3/1 strength training program",
         theme_color: "#2196f3",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     })
   ],
